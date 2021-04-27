268fc411e5acc6da582f9c0531189fc01d0011da
sunwook1218 <sunwook1218@naver.com>
2021-04-27 19:33:00 +0900

    first Application commit

---
+++ app/src/main/java/ko/co/ksign/myapplication/NewWordActivity.kt
@@ -0,0 +1,39 @@
+package ko.co.ksign.myapplication
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import ko.co.ksign.myapplication.viewmodel.WordViewModel
+import ko.co.ksign.myapplication.viewmodel.WordViewModelFactory
+
+class NewWordActivity : AppCompatActivity() {
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_word)
+        val editWordView = findViewById<EditText>(R.id.edit_word)
+
+        val button = findViewById<Button>(R.id.button_save)
+        button.setOnClickListener {
+            val replyIntent = Intent()
+            if (TextUtils.isEmpty(editWordView.text)) {
+                setResult(Activity.RESULT_CANCELED, replyIntent)
+            } else {
+                val word = editWordView.text.toString()
+                replyIntent.putExtra(EXTRA_REPLY, word)
+                setResult(Activity.RESULT_OK, replyIntent)
+            }
+            finish()
+        }
+    }
+
+    companion object {
+        const val EXTRA_REPLY = "com.example.android.wordlistsql.REPLY"
+    }
+}
